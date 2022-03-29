d4e076b934529a743795d4847936b35308058783
Manh Nguyen <martinruan2001@gmail.com>
2022-03-28 21:25:20 -0400

    Forgot password api

--- modules/auth/index.js
+++ modules/auth/index.js
@@ -1,7 +1,9 @@
 const userModel = require("./model")
 const template = require("../../template")
+const nodemailer = require('nodemailer')
 const { hashMd5, signToken, verifyToken } = require("../utils");
-
+const { reset } = require("nodemon");
+ 
 const handlers = {
   async signIn(req, res, next) {
     try {
@@ -12,32 +14,32 @@ const handlers = {
       if (!userName) {
         throw new Error("Missing Username!");
       }
-
+ 
       let hashedPassword = hashMd5(String(password)); // crypto pass
-
+ 
       let user = await userModel.findOne({ userName: userName })
-
+      console.log(typeof(user))
       if (!user) {
         throw new Error("Invalid username");
       }
       if (user.password != hashedPassword) {
         throw new Error("Invalid password");
       }
-
+ 
       let userData = user.toObject()
-
+ 
       let accessToken = signToken({ _id: userData, userName: userData.userName })
       userData.accessToken = accessToken
-
+ 
       res.json(template.successRes(userData))
-
-
+ 
+ 
     } catch (error) {
-      error.status = 400
-      next(err)
+      res.status(400)
+      next(error)
     }
   },
-
+ 
   async signUp(req, res, next) {
     try {
       let data = req.body
@@ -48,35 +50,102 @@ const handlers = {
       ) {
         throw new Error("Invalid password! Password must be between 6 and 30");
       }
-
-
+ 
+ 
       const existedUser = await userModel.findOne({ userName: data.userName })
       if(existedUser) {
         throw new Error ("Username has already existed")
       }
-
+ 
       const usedEmail = await userModel.findOne({ email: data.email })
-
+ 
       if(usedEmail) {
         throw new Error ("Email has already been used")
       }
-
+ 
       formattedData.password = hashMd5(data.password)
       formattedData.email = String(data.email).toLowerCase().trim();
       formattedData.userName = String(data.userName).toLowerCase().trim();
-
-
+ 
+ 
       let user = await userModel.create(formattedData)
       let userData = user.toObject()
-      let accessToken = signToken({ _id: userData, userName: userData.userName });
-      userData.accessToken = accessToken;
-
       res.json(template.successRes(userData));
     } catch (error) {
-      error.status = 400
+      res.status(400)
       next(error);
+    }
+  },
+ 
+  async forgotPassword(req, res, next) {
+   
+    const {email} = req.body
+    try {
+ 
+      let user = await userModel.findOne({ email: email})
+      if(!user) {
+        throw new Error("Email is not registered")
+      }
+      let userData = user.toObject()
+      let testAccount = await nodemailer.createTestAccount()
+ 
+      console.log('this')
+      let token = signToken({ _id: userData, userName: userData.userName })
+      let transport = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD
+        }
+      })
+      const dataRes = {
+        from: process.env.EMAIL_USERNAME,
+        to: email,
+        subject: "Linguistech Recover Password",
+        text: "abcxyz",
+        html: `
+          <h2>Please click on given link to reset your password</h2>
+          <p>${process.env.CLIENT_URL}/reset-password/${token}</p>
+        `
+ 
+      }
+      let response = await new Promise((resolve, reject) => {
+        transport.sendMail(dataRes, (err, info) => {
+          if(err) {
+            throw new Error(err)
+          }
+          resolve(info.response)
+        })
+      })
+      res.json(template.successRes(response))
+    } catch (error) {
+      res.status(400)
+      next(error)
+    }
+  },
+ 
+  async resetPassword(req, res, next) {
+    const { token, email, password } = req.body
+    if(!verifyToken(token)) {
+      res.status(400)
+      next(new Error("Invalid access token!"));
+      return
+    }
+    try {
+      let newPassword = hashMd5(password)
+      var myquery = { email: email };
+      var newvalues = { $set: {password: newPassword } };
+      let respone = await userModel.updateOne(
+        myquery,
+        newvalues
+      )
+      res.json(template.successRes(respone))
+    } catch (error) {
+      res.status(400)
+      next(error)
     }
   }
 }
-
+ 
 module.exports = handlers
+
